708c9c9826109d51d5763f015ee920bd36e844a4
Gabriel Fortuny <92552031+gabrielfortuny@users.noreply.github.com>
2025-09-04 00:59:21 -0400

    database

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
 
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "dev"
+    SQLALCHEMY_DATABASE_URI = (
+        os.environ.get("DATABASE_URL") or f"sqlite:///{BASE_DIR / 'app.db'}"
+    )
