06a12250472445e83c6c1f83359ae9ae1620de79
Gabriel Fortuny <92552031+gabrielfortuny@users.noreply.github.com>
2025-09-07 15:16:56 -0400

    fixed username collision bug

--- app/forms.py
+++ app/forms.py
@@ -29,7 +29,7 @@ class RegistrationForm(FlaskForm):
             sa.select(User).where(func.lower(User.username) == username.data.lower())
         )
         if user is not None:
-            raise ValidationError(f"Username {username.data} is taken!")
+            raise ValidationError(f"Username {username.data} is unavailable.")
 
     def validate_email(self, email):
         user = db.session.scalar(sa.select(User).where(User.email == email.data))
@@ -41,3 +41,15 @@ class EditProfileForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
     submit = SubmitField("Submit")
+
+    def __init__(self, original_username, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = db.session.scalar(
+                sa.select(User).where(User.username == username.data)
+            )
+            if user is not None:
+                raise ValidationError(f"Username {username.data} is unavailable.")
